5cdeca795e6efa659031f40da80d5ff211859232
Meryll Blanchet <mblanche@adobe.com>
2019-10-28 16:10:33 -0700

    Refactored the logic :-)

--- actions/hello.js
+++ actions/hello.js
@@ -1,6 +1,6 @@
 function main(params) {
     console.log('main called with params:', params)
-    return { payload: 'Hello ' + params.name + ', how are you?' };
+    return { payload: 'Hello ' + params.name };
 }
 
 exports.main = main;
