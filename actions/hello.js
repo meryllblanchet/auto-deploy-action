970706e2f7f8672c468fa3cb12d0d0b16a2842ae
Meryll Blanchet <mblanche@adobe.com>
2019-10-28 06:10:46 -0700

    Refactoring
    Reviewed the feature

--- actions/hello.js
+++ actions/hello.js
@@ -1,6 +1,6 @@
 function main(params) {
     console.log('main called with params:', params)
-    return { payload: 'Hello ' + params.name };
+    return { payload: 'Hello ' + params.name + ', how are you?' };
 }
 
 exports.main = main;
